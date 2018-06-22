d651de4786682340b4fd3770a31a260e4f0af321
James McKinney <james@slashpoundbang.com>
2018-06-21 23:11:11 -0400

    Rename package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(  # noqa: E131
-    name='ocds-documentation-support',
+    name='ocdsdocumentationsupport',
     version='0.0.0',
     packages=find_packages(),
     entry_points='''
